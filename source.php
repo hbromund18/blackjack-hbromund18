eb19f36d5560452008100a2bcd692155c794fd66
hbromund18 <hbromund18@amdg.brebeuf.org>
2017-02-09 09:44:54 -0500

    WORKING

---
+++ source.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+if the first letter is a:
+
+C : Clubs
+H : Hearts
+D : Dimond
+S : Spades
+
+The second letter/number will dictate the card of the suite
+
+
+
+
+*/
+
+
+array (($masterlist = ['CA', 'C2', 'C3', 'C4']);
