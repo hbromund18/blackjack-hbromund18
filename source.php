8637b5f37131a069d8d0fcbd76526847f0d56fdb
hbromund18 <hbromund18@amdg.brebeuf.org>
2017-02-10 08:44:20 -0500

    WORKING

--- source.php
+++ source.php
@@ -16,27 +16,26 @@ The second letter/number will dictate the card of the suite
 
 */
 
-$shuffled = array([]);
 array ($masterlist =  ['CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CK', 'CQ',
                        'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HK', 'HQ',
                        'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DK', 'DQ',
                        'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SK', 'SQ']);
 
+
+if (isset($_SESSION)){
+    if (!isset($_SESSION ['shuffledDeck'])) {
+        shuffle($masterlist);
+    }
+    else {
+        var_dump( $_SESSION['shuffledDeck']);
+    }
+}
+
+
+$shuffled = array([]);
+
+
 #var_dump ($masterlist);
 
 
-function shuffler ($masterlist){
-    shuffle($masterlist);
-    #var_dump ($masterlist);
-    $_SESSION['shuffledDeck'] = $masterlist;
-
-}
-
-function start($masterlist){
-    shuffler ($masterlist);
-}
-
-start ($masterlist);
-
-var_dump($_SESSION['shuffledDeck']);
 
